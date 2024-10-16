a5b1b9a16408ca9a8577ba3e64d9d9a0686783b9
lime <kant.yosz@gmail.com>
2024-10-16 14:24:33 +0200

    test for commit - git

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //primitive variables in java:
+        int a = 5;
+        char b = 'l';
+        long c = 400;
+        double d = 3.2;
+
+
+        String Yoooo = "Valorant";
+        System.out.println(Yoooo.toUpperCase()); //prints string yoooo as Upper Case letter -> output: "VALORANT"
+        addExclamationPoint("deez"); //calls upon a "Method" we made listed below
+    }
+
+    public static void addExclamationPoint(String s) {
+        System.out.println(s + "!");
+    }
+
+}
