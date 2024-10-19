2b17b22cce7492f9178d2c30af73f50b1792447b
lime <kant.yosz@gmail.com>
2024-10-19 18:04:32 +0200

    testing comments

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        //create long legume construction
-        int legume_length = 16;
-        String legume = "=";
-        for (int x = 0; x < legume_length; x++) {
-            legume = legume + "=";
-        }
-        System.out.println("8" + legume + "D");
 
     }
 }
