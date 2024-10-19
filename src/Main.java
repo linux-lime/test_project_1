80ecf50de0069728849c474030f3b897f063120c
lime <kant.yosz@gmail.com>
2024-10-19 18:19:56 +0200

    Clearing the code + Adding basic data types

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
 
     public static void main(String[] args) {
+        //primitive Datentypen
+        //Datentypen für ganze Zahlen
+        byte a = 0;       //alle ganzen zahlen von -128 zu +127 da 255 Zahlen + 0
+        short b = 5;      //ca. -32T zu +32T
+        int c = 3;        //ca -2M zu +2M
+        long d = 355;     //ca -9 Trillionen zu +9 Trillion
+
+        //Datentypen für rationalte Zahlen
+        float x = 3.5f;     //angabe muss mit f beendet werden
+        double y = 2.3d;    //angabe muss mit d beendet werden
+
+        boolean e = true;   //bool'scher Wert
+
+        char Grade = 'k';   //text speicher
 
     }
 }
